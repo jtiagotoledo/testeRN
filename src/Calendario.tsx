64414ce771cfcfb290eb257cbe4e1d3e0ab53593
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2023-09-09 16:07:38 -0300

    adicionado dias da semana em português no calendário

--- src/Calendario.tsx
+++ src/Calendario.tsx
@@ -1,6 +1,15 @@
 import React, {useState} from 'react';
 import {Calendar, LocaleConfig} from 'react-native-calendars';
 
+LocaleConfig.locales.br = {
+  monthNames: ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"],
+  monthNamesShort: ["Jan.","Fev.","Mar","Abr","Mai","Jun","Jul.","Ago","Set.","Out.","Nov.","Dez."],
+  dayNames: ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"],
+  dayNamesShort: ["Dom.", "Seg.", "Ter.", "Qua.", "Qui.", "Sex.", "Sáb."]
+};
+
+LocaleConfig.defaultLocale = "br";
+
 const Calendario = () => {
   const [selected, setSelected] = useState('');
 
@@ -8,6 +17,7 @@ const Calendario = () => {
     <Calendar
       onDayPress={day => {
         setSelected(day.dateString);
+        console.log(day.dateString)
       }}
       markedDates={{
         [selected]: {selected: true, disableTouchEvent: true}
