9a0fa20cd357961d1b9933c86683646616e13f4f
Tiago Toledo <jtiagotoledo@gmail.com>
2023-09-26 14:32:38 -0300

    adicionado tela de notas

---
+++ src/HeaderNotas.tsx
@@ -0,0 +1,68 @@
+import React, { useContext } from 'react';
+import {StyleSheet,View} from 'react-native';
+import { Header as HeaderRNE} from '@rneui/themed';
+import { TouchableOpacity } from 'react-native';
+import {Icon} from './Icon'
+import {Context} from "./data/Provider";
+import Globais from './Globais';
+
+type HeaderComponentProps = {
+title: string;
+view?: string;
+};
+
+const HeaderNotas: React.FunctionComponent<HeaderComponentProps> = (props) => {
+
+    const {setModalDelAluno} = useContext(Context);
+    
+    return (
+        <HeaderRNE
+        backgroundColor = {Globais.corPrimaria}
+        style={styles.headerContainer}
+        leftComponent={
+            <View style={styles.headerRight}>
+                <TouchableOpacity>
+                <Icon name="menu" color="white" size={20}/>
+                </TouchableOpacity>
+            </View>
+        }
+        rightComponent={
+            <View style={styles.headerRight}>
+                <TouchableOpacity onPress={()=>setModalDelAluno(true)}>
+                <Icon name="bin" color="white" size={20}/>
+                </TouchableOpacity>
+            </View>
+        }
+        centerComponent={{ text: 'Notas', style: styles.heading }}
+        />
+    
+    );
+};
+
+const styles = StyleSheet.create({
+headerContainer: {
+  alignItems: 'center',
+  backgroundColor: '#397af8',
+  marginBottom: 20,
+  width: '100%',
+  paddingVertical: 15,
+  
+},
+heading: {
+  color: 'white',
+  fontSize: 22,
+  fontWeight: 'bold',
+},
+headerRight: {
+  display: 'flex',
+  flexDirection: 'row',
+  marginTop: 5,
+},
+subheaderText: {
+  color: 'white',
+  fontSize: 16,
+  fontWeight: 'bold',
+},
+});
+
+export default HeaderNotas;
--- src/Notas.tsx
+++ src/Notas.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View, Button } from "react-native"
+import Globais from "./Globais";
+import HeaderNotas from "./HeaderNotas";
+import FlatListClasses from "./FlatListClasses";
 
 const Notas = () =>{
     return(
-        <View>
-            <Text>Notas</Text>
+        <View style={styles.container}>
+            <HeaderNotas title="Notas"></HeaderNotas>
+            <FlatListClasses></FlatListClasses>
+            <Button title='add NOTAS'></Button>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+      backgroundColor: Globais.corSecundaria,
+      flex:1,
+    },
+    text:{
+        fontSize:20,
+        padding:5,
+        color: Globais.corTextoEscuro,
+      },
+});
 
 export default Notas;
