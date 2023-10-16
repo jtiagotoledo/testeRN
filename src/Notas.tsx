124ebe9b9257cc89087a9d21ba398ebef637224e
Tiago Toledo <jtiagotoledo@gmail.com>
2023-10-16 13:50:56 -0300

    teste de assyncStorage

--- src/Notas.tsx
+++ src/Notas.tsx
@@ -1,20 +1,49 @@
-import React from "react";
-import { StyleSheet, View, Button } from "react-native"
+import React, { useState } from "react";
+import { StyleSheet, View, Button, TextInput, Text } from "react-native"
 import Globais from "./Globais";
 import HeaderNotas from "./HeaderNotas";
 import FlatListClasses from "./FlatListClasses";
-import TesteStorage from "./TesteStorage";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Notas = () =>{
-    return(
-        <View style={styles.container}>
-            <HeaderNotas title="Notas"></HeaderNotas>
-            <FlatListClasses></FlatListClasses>
-            <Button title='add NOTAS'></Button>
-            <TesteStorage></TesteStorage>
-        </View>
-    )
+
+  const[lista,setLista]=useState('');
+  const[valor,setValor]=useState('');
+
+
+  const storeData = async () => {
+      try {
+        const jsonValue = JSON.stringify(valor);
+        await AsyncStorage.setItem('my-key', jsonValue);
+      } catch (e) {
+        // saving error
+      }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('my-key');
+      console.log(jsonValue);
+      setLista(jsonValue||'')
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  return(
+      <View style={styles.container}>
+          <HeaderNotas title="Notas"></HeaderNotas>
+          <FlatListClasses></FlatListClasses>
+          <Button title='salvar valores' onPress={()=>storeData()}></Button>
+          <TextInput placeholder="digite" onChangeText={(item)=>setValor(item)}></TextInput>
+          <Button title='pegar valores' onPress={()=>getData()}></Button>
+          <Text>{lista}</Text>
+      </View>
+  )
 }
+
+
 
 const styles = StyleSheet.create({
     container:{
